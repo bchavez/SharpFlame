5a000902784d0f019ba37f88477627a72c30d66f
René Kistl <rene@kistl.at>
2014-02-12 05:20:45 +0100

    Remove more occurences of Microsoft.VisualBasic.

---
+++ source/SharpFlame/ExtensionsForString.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace SharpFlame
+{
+    public static class ExtensionsForString
+    {
+        /*
+         * Returns a string with x chars from the left
+         */
+        public static string Left (this string input, int len)
+        {
+            return input.Substring (0, Math.Min (input.Length, len));
+        }
+        /*
+         * Returns a string with x chars from the right
+         */
+        public static string Right (this string input, int len)
+        {
+            if (input.Length <= len) {
+                return input;
+            }
+            return input.Substring (input.Length - len, len);
+        }
+
+        public static string Format2 (this string format, params object[] args)
+        {
+            return string.Format (format, args);
+        }
+
+        public static string CombinePathWith (this string path1, string path2, bool endWithPathSeporator = false)
+        {
+            if (endWithPathSeporator)
+                return Path.Combine (path1, path2) + Path.DirectorySeparatorChar;
+
+            return Path.Combine (path1, path2);
+        }
+    }
+}
+
