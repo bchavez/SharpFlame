cc2385fa4f7d3de29efa777b02df70539a22e86d
bchavez <bchavez@bitarmory.com>
2014-02-12 23:52:03 -0800

    * Adding LevParserTests.cs

---
+++ source/SharpFlame.Tests/Parser/LevParserTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SharpFlame.Core.Parsers.Lev;
+using Sprache;
+
+namespace SharpFlame.Tests.Parser
+{
+    [TestFixture]
+    public class LevParseTests
+    {
+        [Test]
+        public void can_parse_level()
+        {
+
+            var data = "level   test_flame-T1";
+
+            var result = LevGrammar.Level.Parse(data);
+
+            result.Should().Be("test_flame-T1");
+        }
+
+        [Test]
+        public void can_parse_player()
+        {
+            var data = "players 2";
+
+            var result = LevGrammar.Players.Parse(data);
+
+            result.Should().Be(2);
+        }
+    }
+
+}
