7efe97975e678228d765eab61b55f65ae3b443b2
René Kistl <rene@kistl.at>
2014-02-16 00:31:11 +0100

    Adds new files, from the .lev and .ini refactor.

---
+++ source/SharpFlame.Tests/Parser/GeneralTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using SharpFlame.Core.Parsers;
+using Sprache;
+
+namespace SharpFlame.Tests.Parser
+{
+    [TestFixture]
+    public class GeneralTests
+    {
+        [Test]
+        public void CanParseMultilineComment()
+        {
+            var data = @"/*** Test123
+Test123
+***/
+
+level    Sk-Rush-T1
+";
+            var result = General.MultilineComment.Parse (data);
+            result.Should ().Be ("** Test123\nTest123\n**");
+        }
+
+        [Test]
+        public void CanParseSingleLineComment()
+        {
+            var data = @"// Test123
+
+                data        ""wrf/vidmem.wrf""";
+            var result = General.SingleLineComment.Parse (data);
+            result.Should ().Be (" Test123");
+        }
+    }
+}
+
