0018992244575bc8ce002b7b0c18791f2a3424bf
bchavez <bchavez@bitarmory.com>
2015-07-25 15:33:29 -0700

    Updating major references

--- source/SharpFlame/Gui/Sections/ObjectTab.cs
+++ source/SharpFlame/Gui/Sections/ObjectTab.cs
@@ -1,24 +1,18 @@
 using System;
-using System.Collections;
 using System.Linq;
 using Appccelerate.EventBroker;
 using Appccelerate.EventBroker.Handlers;
 using Eto;
-using Eto.Drawing;
 using Eto.Forms;
 using Ninject;
 using SharpFlame.Core;
 using SharpFlame.Domain;
 using SharpFlame.FileIO;
-using SharpFlame.Gui.Controls;
-using SharpFlame;
 using SharpFlame.Mapping;
-using SharpFlame.Mapping.Objects;
 using SharpFlame.Mapping.Tools;
 using SharpFlame.Maths;
 using SharpFlame.MouseTools;
 using SharpFlame.Settings;
-
 
 namespace SharpFlame.Gui.Sections
 {
@@ -54,86 +48,6 @@ namespace SharpFlame.Gui.Sections
 	    public ObjectTab ()
 	    {
 		    XomlReader.Load(this);
-		    /*var mainLayout = new DynamicLayout ();
-
-            var nLayout0 = new DynamicLayout { Padding = Padding.Empty };
-            var nLayout1 = new DynamicLayout ();
-            nLayout1.AddRow(
-                new Label { Text = "Type:", VerticalAlign = VerticalAlign.Middle },
-                new Label { Text = "A0CyborgFactory (Cyborg Factory)", VerticalAlign = VerticalAlign.Middle });
-           
-
-            nLayout0.AddRow (nLayout1);
-
-            PlayerSelector playerSelector;
-            if (this.Platform.IsWinForms)
-            {
-                playerSelector = new PlayerSelectorWinforms (Constants.PlayerCountMax);
-            } else
-            {
-                playerSelector = new PlayerSelector (Constants.PlayerCountMax);
-            }
-            playerSelector.SelectedPlayer = "S";
-
-            var nLayout2 = new DynamicLayout { Padding = Padding.Empty };
-            nLayout2.AddRow(new Label { Text = "Player", VerticalAlign = VerticalAlign.Middle }, 
-                            TableLayout.AutoSized(playerSelector)
-            );
-
-            nLayout0.AddRow (nLayout2);
-
-            var nLayout3 = new DynamicLayout ();
-            var nLayout4 = new DynamicLayout { Padding = Padding.Empty };
-            nLayout4.AddRow(TableLayout.AutoSized(new NumericUpDown { MaxValue = 360, MinValue = 0, Value = 0, Size = new Size(-1, -1) }),
-                            new Label { Text = "(0-359)", VerticalAlign = VerticalAlign.Middle });
-
-            nLayout3.AddRow(new Label { Text = "Rotation:", VerticalAlign = VerticalAlign.Middle },
-                            nLayout4,
-                            new Button { Text = "Realign" });                            
-
-            nLayout3.AddRow(new Label { Text = "ID:", VerticalAlign = VerticalAlign.Middle },
-                            new TextBox { Text = "1200", Enabled = false },
-                            new Button { Text = "Flatten Terrain" });
-
-            nLayout3.AddRow(new Label { Text = "Label:", VerticalAlign = VerticalAlign.Middle },
-                            new TextBox ());
-
-            nLayout3.AddRow(new Label { Text = "Health%:", VerticalAlign = VerticalAlign.Middle },
-                            TableLayout.AutoSized(new NumericUpDown { MinValue = 0, MaxValue = 100, Value = 100, Size = new Size(-1, -1) }));
-
-            nLayout0.AddRow (nLayout3);
-
-            mainLayout.AddRow (null, nLayout0, null);
-
-            var nLayout5 = new DynamicLayout ();
-            nLayout5.AddRow(TableLayout.AutoSized (new Button { Text = "Convert Templates To Design" }),
-                            new CheckBox { Text = "Designables Only", Checked = true });
-            mainLayout.AddRow (null, nLayout5, null);
-
-            var nLayout6 = new DynamicLayout ();
-            nLayout6.AddRow (new Label { Text = "Type", VerticalAlign = VerticalAlign.Middle }, new ComboBox ());
-            nLayout6.AddRow (new Label { Text = "Body", VerticalAlign = VerticalAlign.Middle }, new ComboBox ());
-            nLayout6.AddRow (new Label { Text = "Propulsion", VerticalAlign = VerticalAlign.Middle }, new ComboBox ());
-
-            var radio0 = new RadioButton { Text = "0" };
-            var radio1 = new RadioButton (radio0) { Text = "1" };
-            var radio2 = new RadioButton (radio1) { Text = "2" };
-            var radio3 = new RadioButton (radio2) { Text = "3" };
-
-            var nLayout7 = new DynamicLayout { Padding = Padding.Empty };
-            nLayout7.AddRow(new Label { Text = "Turrets", VerticalAlign = VerticalAlign.Middle }, radio1);
-            nLayout6.AddRow(nLayout7, new ComboBox ());
-    
-            var nLayout8 = new DynamicLayout { Padding = Padding.Empty };
-            nLayout8.AddRow(radio0, radio2);
-            nLayout6.AddRow(nLayout8, new ComboBox ());
-
-            nLayout6.AddRow(radio3, new ComboBox ());
-
-            mainLayout.AddRow(null, nLayout6, null);
-            mainLayout.Add (null);
-
-            Content = mainLayout;*/
 	    }
 
 		private StackLayout slTurret1;
@@ -231,26 +145,26 @@ namespace SharpFlame.Gui.Sections
 					return false;
 				}, setValue: null), mode: DualBindingMode.OneWay);
 
-			this.ddlType.SelectedIndexBinding.BindDataContext(getValue: (Map m) =>
-				{
-					if( m.SelectedUnits.Count == 1 )
-					{
-						//left off here.
-						var u = m.SelectedUnits[0];
-						if( u.TypeBase.Type == UnitType.PlayerDroid )
-						{
-							var droidType = (DroidDesign)u.TypeBase;
-							if( droidType.TemplateDroidType != null )
-							{
-								return droidType.TemplateDroidType.Num;
-							}
-						}
-					}
-					return -1;
-				}
-				, setValue: null);
+            //this.ddlType.SelectedIndexBinding.BindDataContext(getValue: (Map m) =>
+            //    {
+            //        if( m.SelectedUnits.Count == 1 )
+            //        {
+            //            //left off here.
+            //            var u = m.SelectedUnits[0];
+            //            if( u.TypeBase.Type == UnitType.PlayerDroid )
+            //            {
+            //                var droidType = (DroidDesign)u.TypeBase;
+            //                if( droidType.TemplateDroidType != null )
+            //                {
+            //                    return droidType.TemplateDroidType.Num;
+            //                }
+            //            }
+            //        }
+            //        return -1;
+            //    }
+            //    , setValue: null);
 
-			this.ddlBody.SelectedKeyBinding.BindDataContext(getValue: (Map m) =>
+            this.ddlBody.SelectedKeyBinding.BindDataContext(getValue: (Map m) =>
 				{
 					if( m.SelectedUnits.Count == 1 )
 					{
@@ -368,10 +282,16 @@ namespace SharpFlame.Gui.Sections
         {
             base.OnLoadComplete(lcEventArgs);
 
-            // Set Mousetool, when we are shown.
-            Shown += delegate {
-                ToolOptions.MouseTool = MouseTool.ObjectSelect;
-            };
+            // Set Mousetool, when we are shown
+            this.Shown += (sender, args) =>
+                {
+                    ToolOptions.MouseTool = MouseTool.ObjectSelect;
+                };
+        }
+
+        public void doShow(object sender, EventArgs e)
+        {
+            ToolOptions.MouseTool = MouseTool.ObjectSelect;
         }
 
 		[EventSubscription(EventTopics.OnMapLoad, typeof(OnPublisher))]
